34ac837896c0a408e83bdab8d1076d1b26deb1e2
unitx01 <dinxmoshxgolad@gmail.com>
2026-06-26 16:56:00 -0700

    Created a simple JS file and html document

---
+++ Basic JavaScript Projects/Project1_expressions_alert/script.js
@@ -0,0 +1,6 @@
+var Sent1 = 'This is the beginning of the string';
+var Sent2 = 'and this is the end of the string';
+var Sents = Sent1 + " " + Sent2;
+
+window.alert(Sent1);
+document.write(Sents);

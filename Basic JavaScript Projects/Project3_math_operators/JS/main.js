721bb3196b7d9a191918aa61ab135c26f51f8980
unitx01 <dinxmoshxgolad@gmail.com>
2026-06-26 19:05:51 -0700

    update

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,13 +1,13 @@
 function y() {
 
     var x = Math.random() * 100 + 5 - 5 * 10 % 20; //Math operation with random
-    var a = 9
-    var b = 2
+    var a = 9 // defines that a has the value of 9
+    var b = 2 // defines that a has the value of 2
 
-    a++;
-    b--;
+    a++; // Increase a by one
+    b--; // Decrease b by one
 
-    var z = x + a - b;
+    var z = x + a - b; // adds the value of x to a detracting b
 
 
 

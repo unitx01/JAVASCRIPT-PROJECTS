6c019c2190bd634ba3aa3486a5ba454fcc3f903a
unitx01 <dinxmoshxgolad@gmail.com>
2026-06-26 17:35:04 -0700

    added comments

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,5 @@
-function My_First_Function() {
-    var str = "A Cat!";
-    str += " A beautiful cat!";
-    document.getElementById("Button_Text").innerHTML = str;
+function My_First_Function() { //variable called str and assign a string
+    var str = "A Cat!"; //defines the variable str with this text
+    str += " A beautiful cat!"; // using the += to add more text to the string
+    document.getElementById("Button_Text").innerHTML = str;// finds the ID elemnt "Button_Text" and replaces the value of str
 }

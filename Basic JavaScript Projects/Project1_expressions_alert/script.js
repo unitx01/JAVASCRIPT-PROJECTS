58b67454b9d23c68395fd2b9af2cb53fae9d1bfa
unitx01 <dinxmoshxgolad@gmail.com>
2026-06-26 17:28:58 -0700

    added expression and comments.

--- Basic JavaScript Projects/Project1_expressions_alert/script.js
+++ Basic JavaScript Projects/Project1_expressions_alert/script.js
@@ -1,6 +1,9 @@
-var Sent1 = 'This is the beginning of the string';
-var Sent2 = 'and this is the end of the string';
-var Sents = Sent1 + " " + Sent2;
+var Sent1 = 'Do you know how much is 2+2?';// Variable sent1 displays this text
+var Sent2 = 'This much!'; // Variable sent2 displays this text
+var Sents = Sent1 + " " + Sent2; // combines sent1 and sent2 while spacing them
+var math = (2+2); // simple function displaying the result of 2+2
+
 
 window.alert(Sent1);
+window.alert(math);
 document.write(Sents);
